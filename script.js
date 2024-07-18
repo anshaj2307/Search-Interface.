1636744d9c67145589ab9bc646051d67399fcda0
anshaj2307 <160524216+anshaj2307@users.noreply.github.com>
2024-07-18 12:17:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+document.getElementById('dropdownButton').addEventListener('click', function() {
+    document.getElementById('dropdownMenu').classList.toggle('hidden');
+  });
+
+  document.querySelectorAll('#dropdownMenu li').forEach(function(item) {
+    item.addEventListener('click', function() {
+      const selectedText = this.textContent;
+      document.getElementById('dropdownText').textContent = selectedText;
+      document.getElementById('searchInput').placeholder = `Search ${selectedText}`;
+      document.getElementById('dropdownMenu').classList.add('hidden');
+    });
+  });
+
+
+  window.tailwind.config = {
+    darkMode: ['class'],
+    theme: {
+      extend: {
+        colors: {
+          border: 'hsl(var(--border))',
+          input: 'hsl(var(--input))',
+          ring: 'hsl(var(--ring))',
+          background: 'hsl(var(--background))',
+          foreground: 'hsl(var(--foreground))',
+          primary: {
+            DEFAULT: 'hsl(var(--primary))',
+            foreground: 'hsl(var(--primary-foreground))'
+          },
+          secondary: {
+            DEFAULT: 'hsl(var(--secondary))',
+            foreground: 'hsl(var(--secondary-foreground))'
+          },
+          destructive: {
+            DEFAULT: 'hsl(var(--destructive))',
+            foreground: 'hsl(var(--destructive-foreground))'
+          },
+          muted: {
+            DEFAULT: 'hsl(var(--muted))',
+            foreground: 'hsl(var(--muted-foreground))'
+          },
+          accent: {
+            DEFAULT: 'hsl(var(--accent))',
+            foreground: 'hsl(var(--accent-foreground))'
+          },
+          popover: {
+            DEFAULT: 'hsl(var(--popover))',
+            foreground: 'hsl(var(--popover-foreground))'
+          },
+          card: {
+            DEFAULT: 'hsl(var(--card))',
+            foreground: 'hsl(var(--card-foreground))'
+          },
+        },
+      }
+    }
+  }
